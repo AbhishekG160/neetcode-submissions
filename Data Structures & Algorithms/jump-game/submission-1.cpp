2ee66cf474592e8eae3734f92617c90730f3821e
Abhishek Gupta <144529860+AbhishekG160@users.noreply.github.com>
2026-09-08 21:45:04 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        // vector<bool> reachable(n, false);
+        // reachable[n-1] = true;
+        // for(int i=n-2; i>=0; i--){
+        //     int limit = min(n-1, i+nums[i]);
+        //     for(int j=i+1; j<=limit; j++){
+        //         if(reachable[j]){
+        //             reachable[i] = true;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return reachable[0];
+        int maxreach = 0;
+        for(int i=0; i<n; i++){
+            if(i > maxreach) 
+                return false;
+            maxreach = max(maxreach, i+nums[i]);
+            if(maxreach >= n-1)
+                return true;
+        }
+        return false;
+    }
+};
