0e22e1fb9231f598a08cb92acd08db426bc889ec
Abhishek Gupta <144529860+AbhishekG160@users.noreply.github.com>
2026-09-09 19:30:26 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        vector<char> v;
+        for(char c:s){
+            if(c == ')'){
+                if(!v.empty() && v.back() == '(') v.pop_back();
+                else return false;
+            }
+            else if(c == '}'){
+                if(!v.empty() && v.back() == '{') v.pop_back();
+                else return false;
+            }
+            else if(c == ']'){
+                if(!v.empty() && v.back() == '[') v.pop_back();
+                else return false;
+            }
+            else v.push_back(c);
+        }
+        return v.empty();
+    }
+};
